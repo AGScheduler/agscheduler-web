ea2f9244efd82e0adeca5028ce72fbdfa00249b8
kwkw <wei497896143@yeah.net>
2024-03-22 00:20:57 +0000

    build: add deploy workflow

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,10 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
