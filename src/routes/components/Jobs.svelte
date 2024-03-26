e136009ef20cdc7c11d600052eefe0736efb63df
kwkw <wei497896143@yeah.net>
2024-03-26 15:19:52 +0800

    feat(Jobs): add deleteAllJobs

--- src/routes/components/Jobs.svelte
+++ src/routes/components/Jobs.svelte
@@ -4,9 +4,11 @@
 
 	import { Badge } from '$lib/components/ui/badge';
 	import { Button } from '$lib/components/ui/button';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
 	import * as Table from '$lib/components/ui/table';
 	import LoaderCircle from 'lucide-svelte/icons/loader-circle';
 	import RotateCw from 'lucide-svelte/icons/rotate-cw';
+	import Trash2 from 'lucide-svelte/icons/trash-2';
 	import dayjs from 'dayjs';
 
 	import JobsActions from './JobsActions.svelte';
@@ -16,6 +18,7 @@
 	import { fetchWithTimeout } from '../utils.js';
 
 	let isLoading = false;
+	let showDeleteADialog = false;
 
 	let perPage = 10;
 	let page = 1;
@@ -51,12 +54,35 @@
 			});
 	}
 
+	function deleteAllJobs() {
+		isLoading = true;
+
+		fetchWithTimeout($host + '/scheduler/jobs', { method: 'DELETE' })
+			.catch((error) => {
+				toast.error('' + error);
+			})
+			.finally(() => {
+				isLoading = false;
+				showDeleteADialog = false;
+				fetchJobs();
+			});
+	}
+
 	onMount(() => {
 		fetchJobs();
 	});
 </script>
 
 <div class="flex items-center justify-end space-x-2">
+	<Button
+		variant="ghost"
+		size="icon"
+		class="h-6 w-6 p-0"
+		on:click={() => (showDeleteADialog = true)}
+		disabled={isLoading}
+	>
+		<Trash2 class="h-4 w-4" />
+	</Button>
 	<Button variant="ghost" size="icon" class="h-6 w-6 p-0" on:click={fetchJobs} disabled={isLoading}>
 		{#if isLoading}
 			<RotateCw class="h-4 w-4 animate-spin" />
@@ -124,3 +150,21 @@
 		<Pagination bind:count={jobs.length} bind:page />
 	{/if}
 {/if}
+
+<AlertDialog.Root bind:open={showDeleteADialog}>
+	<AlertDialog.Content>
+		<AlertDialog.Header>
+			<AlertDialog.Title>Are you sure absolutely sure?</AlertDialog.Title>
+			<AlertDialog.Description>This action cannot be undone.</AlertDialog.Description>
+		</AlertDialog.Header>
+		<AlertDialog.Footer>
+			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+			<Button variant="destructive" on:click={deleteAllJobs}>
+				{#if isLoading}
+					<LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
+				{/if}
+				Delete
+			</Button>
+		</AlertDialog.Footer>
+	</AlertDialog.Content>
+</AlertDialog.Root>
