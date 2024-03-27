d8ef78594200999a0962e2b39b05e674b4b39288
kwkw <wei497896143@yeah.net>
2024-03-27 15:56:25 +0800

    feat: support dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,11 @@
 	import '../app.pcss';
 	import './styles.css';
 	import { Toaster } from 'svelte-sonner';
+
+	import { ModeWatcher } from 'mode-watcher';
 </script>
+
+<ModeWatcher />
 
 <Toaster />
 
