806ff4a7549b3934896c4076e80cf1fabd94210a
kwkw <wei497896143@yeah.net>
2024-03-27 15:57:34 +0800

    feat: support dark mode (#10)

--- src/routes/components/Info.svelte
+++ src/routes/components/Info.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { toast } from 'svelte-sonner';
+	import { toggleMode } from 'mode-watcher';
 
 	import { Badge } from '$lib/components/ui/badge';
 	import { Button } from '$lib/components/ui/button';
 	import { Label } from '$lib/components/ui/label';
+	import { Separator } from '$lib/components/ui/separator';
 	import { Switch } from '$lib/components/ui/switch';
+	import MoonStar from 'lucide-svelte/icons/moon-star';
 	import Settings from 'lucide-svelte/icons/settings';
+	import Sun from 'lucide-svelte/icons/sun';
 
 	import InfoDetails from './InfoDetails.svelte';
 	import { info, host } from '../stores.js';
@@ -51,11 +55,21 @@
 </script>
 
 <div class="grid gap-1" style="margin-left: auto;">
-	<div class="flex items-center space-x-2">
+	<div class="flex items-center space-x-1">
 		<Badge variant="secondary">{$host}</Badge>
+		<InfoDetails on:fetchInfo={fetchInfo} />
 	</div>
 	<div class="flex items-center justify-end space-x-2">
-		<InfoDetails on:fetchInfo={fetchInfo} />
+		<Button on:click={toggleMode} variant="ghost" size="icon" class="relative h-6 w-6 p-0">
+			<Sun
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+			/>
+			<MoonStar
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
+		<Separator orientation="vertical" />
 		<Button
 			variant="ghost"
 			size="icon"
