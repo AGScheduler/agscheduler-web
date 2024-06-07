d966d67c4a8a0a10361e62233162cf081af9a389
kwkw <wei497896143@yeah.net>
2024-06-07 19:39:38 +0800

    feat(Job): supports `RunJob` `ScheduleJob`

--- src/routes/components/JobsActions.svelte
+++ src/routes/components/JobsActions.svelte
@@ -6,8 +6,10 @@
 	import * as AlertDialog from '$lib/components/ui/alert-dialog';
 	import FilePenLine from 'lucide-svelte/icons/file-pen-line';
 	import LoaderCircle from 'lucide-svelte/icons/loader-circle';
+	import Orbit from 'lucide-svelte/icons/orbit';
 	import Pause from 'lucide-svelte/icons/pause';
 	import Play from 'lucide-svelte/icons/play';
+	import Send from 'lucide-svelte/icons/send';
 	import Trash2 from 'lucide-svelte/icons/trash-2';
 
 	import Job from './Jobs.svelte';
@@ -39,6 +41,40 @@
 			.finally(() => {
 				isLoading = false;
 				dispatch('fetchJobs');
+			});
+	}
+
+	function runOrScheduleJob(action: string) {
+		isLoading = true;
+
+		let newJob: Job = {
+			id: job.id,
+			name: job.name,
+			type: job.type,
+			start_at: job.start_at,
+			interval: job.interval,
+			cron_expr: job.cron_expr,
+			timezone: job.timezone,
+			func_name: job.func_name,
+			args: job.args,
+			timeout: job.timeout,
+			queues: job.queues
+		};
+
+		let path = '/scheduler/job/' + action;
+		fetchWithTimeout($address + path, {
+			method: 'POST',
+			body: JSON.stringify(newJob)
+		})
+			.then(() => {
+				action = action.charAt(0).toUpperCase() + action.slice(1);
+				toast.success(action + ' job success');
+			})
+			.catch((error) => {
+				toast.error('' + error);
+			})
+			.finally(() => {
+				isLoading = false;
 			});
 	}
 
@@ -83,6 +119,34 @@
 >
 	<span class="sr-only">Update</span>
 	<FilePenLine class="h-4 w-4" />
+</Button>
+<Button
+	variant="ghost"
+	size="icon"
+	class="relative h-8 w-8 p-0"
+	on:click={() => runOrScheduleJob('run')}
+	disabled={isLoading}
+>
+	<span class="sr-only">Run</span>
+	{#if isLoading}
+		<LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
+	{:else}
+		<Send class="h-4 w-4" />
+	{/if}
+</Button>
+<Button
+	variant="ghost"
+	size="icon"
+	class="relative h-8 w-8 p-0"
+	on:click={() => runOrScheduleJob('schedule')}
+	disabled={isLoading}
+>
+	<span class="sr-only">Schedule</span>
+	{#if isLoading}
+		<LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
+	{:else}
+		<Orbit class="h-4 w-4" />
+	{/if}
 </Button>
 <Button
 	variant="ghost"
